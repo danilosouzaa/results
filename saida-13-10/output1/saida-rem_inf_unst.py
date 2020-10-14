23767ff01aea27fdf0847ce016fd8501aa99e058
danilosouzaa <danilo.gdc@gmail.com>
2020-10-14 11:50:46 -0300

    13-10

---
+++ saida-13-10/output1/saida-rem_inf_unst.py
@@ -0,0 +1,19 @@
+Welcome to the CBC MILP Solver 
+Version: devel 
+Build Date: Oct  1 2020 
+
+command line - ./cbc /home/danilo/CBCDanilo/bin/input/cbc-test-set/rem_inf_unst.py MaxN 0 passC 1 cuts Off knapsack On heur off preprocess off sec 1200 solve (default strategy 1)
+At line 8
+Unknown image  at line 1 of file /home/danilo/CBCDanilo/bin/input/cbc-test-set/rem_inf_unst.py
+Coin0008I  read with -2 errors
+There were -2 errors on input
+maxNodes was changed from 2147483647 to 0
+passCuts was changed from 20 to 1
+Option for cutsOnOff changed from on to off
+Option for knapsackCuts changed from off to on
+Option for heuristicsOnOff changed from on to off
+Option for preprocess changed from sos to off
+seconds was changed from 1e+100 to 1200
+** Current model not valid
+Total time (CPU seconds):       0.00   (Wallclock seconds):       0.00
+
